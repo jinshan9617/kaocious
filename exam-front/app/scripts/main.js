a2edbb2063bf5221035c8b5cc19e612cb6f7b5ed
Shan Jin <shanx.jin@intel.com>
2013-10-25 18:04:20 +0800

    20131025

---
+++ exam-front/app/scripts/main.js
@@ -0,0 +1,44 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        bootstrap: 'vendor/bootstrap',
+        cookie: '../bower_components/jquery.cookie/jquery.cookie'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        },
+        cookie: {
+            deps: ['jquery']
+        }
+    }
+});
+
+require(['jquery', 'bootstrap', 'cookie'], function ($) {
+    var csrftoken = $.cookie('csrftoken');
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        crossDomain: false, // obviates need for sameOrigin test
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+    $('button').click(function(){
+        $.post(
+            "/answer/1",
+            {
+                'type': "select_question",
+                'options': [1]
+            },
+            function(data){
+                alert(data);
+            }
+        );
+    });
+});
